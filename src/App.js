319423ed73aa9fdc556ea452a79cfcfca2d102ef
Rose Guttman <roseguttman@gmail.com>
2020-02-07 09:50:34 -0800

    class

--- src/App.js
+++ src/App.js
@@ -1,73 +1,101 @@
-import React, {useContext, useState} from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import React, {useContext, useState} from 'react'
+import './App.css'
 import {Input, Button} from 'antd'
 import {Bar} from 'react-chartjs-2'
+import * as moment from 'moment'
+const ButtonGroup = Button.Group;
 
 const context = React.createContext()
 
-
 function App() {
   const [state, setState] = useState({
-    searchTerm:''
-  }) 
+    searchTerm:'',
+    mode:'hourly'
+  })
   return <context.Provider value={{
-    ...state, // Putting all the properties into the context
-    set: v=> setState({...state, ...v}) //Set the state to be a new object, set all the existing states and add new pieces
+    ...state,
+    set: v=> setState({...state, ...v})
   }}>
     <div className="App">
-      <Header>
-        {state.error && <div>{state.error}</div>}
-      </Header>
+      <Header />
+      <Body />
     </div>
-  </context.Provider>;
+  </context.Provider>
 }
+
 
 
 function Header(){
   const ctx = useContext(context)
+  const {loading, searchTerm, mode} = ctx
   return <header className="App-header">
     <Input 
-      style={{height:'3rem', fontSize:'2rem'}} 
-      value={ctx.searchTerm}
+      value={searchTerm} disabled={loading}
       onChange={e=> ctx.set({searchTerm: e.target.value})}
+      style={{height:'3rem',fontSize:'2rem'}} 
       onKeyPress={e=>{
-        if(e.key==='Enter'&& ctx.searchTerm) search(ctx)
+        if(e.key==='Enter' && searchTerm) search(ctx)
       }}
     />
-    <Button style={{margineLeft:5,height:'3rem'}}
+    <Button style={{marginLeft:5,height:'3rem'}}
       onClick={()=> search(ctx)} type="primary"
-      disabled={!ctx.searchTerm}>
+      disabled={!searchTerm} loading={loading}> 
       Search
     </Button>
 
+    <ButtonGroup style={{marginLeft:5, display:'flex'}}>
+        <Button style={{height:'3rem'}} type={mode==='hourly'?'primary':'default'}
+        onClick={()=>ctx.set({mode:'hourly'})}>
+          Hourly
+        </Button>
+        <Button style={{height:'3rem'}} type={mode==='daily'?'primary':'default'}
+        onClick={()=>ctx.set({mode:'daily'})}>
+          Daily
+        </Button>
+    </ButtonGroup>
   </header>
 }
+
 function Body(){
   const ctx = useContext(context)
-  const {error, weather} = ctx
+  const {error, weather, mode} = ctx
   let data
   if(weather){
+    console.log(weather)
     data = {
-      labels: weather.daily.data.map(d=>d.time),
-      datasets: [ weather.daily.data.map(d=>d.temperatureHigh)]
+      labels: weather[mode].data.map(d=>{
+        let format = 'ddd'
+        if(mode==='hourly') format='dd HH:mm'
+        return moment(d.time*1000).format(format)
+      }),
+      datasets: [{
+        label:'Temperature',
+        data: weather[mode].data.map(d=>{
+          if(mode==='hourly') return d.temperature
+          else return (d.temperatureHigh+d.temperatureLow)/2
+        }),
+        backgroundColor: 'rgba(132,99,255,0.2)',
+        borderColor: 'rgba(132,99,255,1)',
+        hoverBackgroundColor: 'rgba(132,99,255,0.4)',
+        hoverBorderColor: 'rgba(132,99,255,1)',
+      }]
     }
   }
+  console.log(data)
   return <div className="App-body">
-    {error && <div className="error">{ctx.error}</div>}
-    {weather && <div>
+    {error && <div className="error">{error}</div>}
+    {data && <div>
       <Bar data={data}
         width={800} height={400}
       />
     </div>}
-    </div>
+  </div>
 }
 
-
 async function search({searchTerm, set}){
-  try{
+  try {
     const term = searchTerm
-    set({searchTerm:'', error:''})
+    set({error:'', loading:true})
 
     const osmurl = `https://nominatim.openstreetmap.org/search/${term}?format=json`
     const r = await fetch(osmurl)
@@ -81,10 +109,10 @@ async function search({searchTerm, set}){
     const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${city.lat},${city.lon}`
     const r2 = await fetch(url)
     const weather = await r2.json()
-    set({weather})
-  } catch(e){
+    set({weather, loading:false, searchTerm:''})
+  } catch(e) {
     set({error: e.message})
-    }
   }
+}
 
 export default App;
